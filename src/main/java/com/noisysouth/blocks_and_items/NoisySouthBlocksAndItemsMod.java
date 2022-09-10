b10cf50ad8d824b50ed0358bff7922ef2a9b5ecd
Noisy South <noisysouth@gmail.com>
2022-09-10 16:38:04 -0500

    step 2 of making the new mod - fixes: - added instance to main class - filled out rest of info - made name shorter to fit better in display area. Changed FROM "Blocks and Items" TO "Blocks N Items"

--- src/main/java/com/noisysouth/blocks_and_items/NoisySouthBlocksAndItemsMod.java
+++ src/main/java/com/noisysouth/blocks_and_items/NoisySouthBlocksAndItemsMod.java
@@ -2,12 +2,16 @@ package com.noisysouth.blocks_and_items;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid=Reference.MODID, name=Reference.MODNAME, version=Reference.VERSION, acceptedMinecraftVersions=Reference.ACCEPTED_MINECRAFT_VERSIONS)
 public class NoisySouthBlocksAndItemsMod {
+	@Instance
+	public static NoisySouthBlocksAndItemsMod instance;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		System.out.println(Reference.MODID + ":preInit");
--- src/main/java/com/noisysouth/blocks_and_items/Reference.java
+++ src/main/java/com/noisysouth/blocks_and_items/Reference.java
@@ -2,7 +2,7 @@ package com.noisysouth.blocks_and_items;
 
 public class Reference {
 	public static final String MODID = "blocks_and_items";
-	public static final String MODNAME = "NoisySouth's Blocks and Items Mod";
+	public static final String MODNAME = "NoisySouth's Blocks N Items Mod";
 	public static final String VERSION = "1.0";
 	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
 }
