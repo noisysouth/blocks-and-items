0fa3e18a8732b92e104f407abdfe75f07b3dcc6f
Noisy South <noisysouth@gmail.com>
2022-09-10 15:59:48 -0500

    files initial state w/ no changes so far; includes 2 helper .bat files for configuring environment

---
+++ setpath2+gradle_eclipse.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+ECHO *** Path before:
+ECHO %PATH%
+ECHO ***
+ECHO *** JAVA_HOME before:
+ECHO %JAVA_HOME%
+ECHO ***
+PAUSE
+SET PATH=C:\Program Files\Java\jdk1.8.0_333\bin
+SET PATH=%PATH%;%SystemRoot%\system32
+SET PATH=%PATH%;%SystemRoot%\System32\Wbem
+SET PATH=%PATH%;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\
+SET PATH=%PATH%;%SYSTEMROOT%\System32\OpenSSH\
+SET PATH=%PATH%;%SYSTEMROOT%\Git\cmd
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_333
+ECHO *** Path after:
+ECHO %PATH%
+ECHO ***
+ECHO *** JAVA_HOME after:
+ECHO %JAVA_HOME%
+ECHO ***
+PAUSE
+CALL .\gradlew eclipse
